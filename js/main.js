c740557b0e129bab121925f0e1e1a462a5e20290
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-06-20 10:16:00 -0500

    Estructura, estilos y eventos para el sub menu

--- js/main.js
+++ js/main.js
@@ -2,18 +2,42 @@ function dropdownFunction() {
   const arrowIcon = document.querySelector("#arrow-icon");
   const contentItems = document.querySelector("#content-items-dropdown");
   const contenedor = document.querySelector(".content-dropdown");
+  const listItems = Array.from(document.querySelectorAll(".list-item"));
+  const subList = document.querySelector(".sub-list");
+
+  // console.log(listItems);
   contenedor.addEventListener("mouseover", () => {
-    console.log("Encima del boton");
+    // console.log("Encima del boton");
     contentItems.classList.remove("unactive");
     contentItems.classList.add("active");
     arrowIcon.classList.remove("rotate");
   });
 
   contenedor.addEventListener("mouseout", () => {
-    console.log("fuera del boton");
+    // console.log("fuera del boton");
     contentItems.classList.remove("active");
     contentItems.classList.add("unactive");
     arrowIcon.classList.add("rotate");
+  });
+
+  //Evento para los sub menu
+  listItems.forEach((listItem) => {
+    listItem.addEventListener("click", () => {
+      // console.log("Click al item");
+      if (subList.className == "sub-list unactive") {
+        subList.classList.remove("unactive");
+      } else {
+        subList.classList.add("unactive");
+      }
+    });
+  });
+
+  subList.addEventListener("mouseover", () => {
+    subList.classList.remove("unactive");
+  });
+
+  subList.addEventListener("mouseout", () => {
+    subList.classList.add("unactive");
   });
 }
 
